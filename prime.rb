c8b9e2fa02ae36b849b4326939ea257c62c45b65
Hanbi Hans Choi <personabe1984@gmail.com>
2018-07-01 23:46:37 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,15 @@
 # Add  code here!
 def prime?(num)
-  prime_check = false
   number_check = num / 2
   
   if num <= 0 || num == 1
-    return prime_check
-  elsif num
-    for counter in 2..num-1 do 
+    return false
+  elsif num == 2
+    return true
+  else
+    for counter in 2..number_check do 
       if num % counter == 0
-        return prime_check
+        
       else
         prime_check = true
       end

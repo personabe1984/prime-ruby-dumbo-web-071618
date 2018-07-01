99d9759166dbc76e8733ed2c9934f37347420014
Hanbi Hans Choi <personabe1984@gmail.com>
2018-07-01 23:38:44 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,12 +4,15 @@ def prime?(num)
   if num <= 0 || num == 1
     return prime_check
   else
-  for counter in 2..num-1 do 
-    if num % counter == 0
-      return prime_check
-    else
-      
-    
+    for counter in 2..num-1 do 
+      if num % counter == 0
+        return prime_check
+      else
+        prime_check = true
+      end
+    end
+  end 
+  return prime_check
 end
 
 puts prime?(105557)

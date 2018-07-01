27a9b0365f078b5342f3f86863504d23b31569dd
Hanbi Hans Choi <personabe1984@gmail.com>
2018-07-01 23:56:52 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(num)
+  if num <= 0 || num == 1
+    return false
+  elsif num == 2
+    return true
+  else
+    for counter in 2..num-1 do 
+      if num % counter == 0
+        return false
+      end
+    end
+    return true
+  end 
+end
+
+
